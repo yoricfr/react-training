82c3a3763b4cced9463e537ecf626d9d17ec75bd
Yoric Mangeart <yoricm@free.fr>
2020-04-03 23:08:20 +0200

    Composition of two componnents

--- pure-react/react-hello/src/index.js
+++ pure-react/react-hello/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function HelloWorld() {
-  return React.createElement(
-   'div',
-    {},
-    'Hello World (bis)!'
-    );
+function Hello() {
+  return <span>Hello</span>;
 }
+
+function World() {
+  return <span>World</span>;
+}
+
+function HelloWorld() {
+  return (
+    <div>
+      <Hello/> <World/>!!! 
+    </div> 
+    ); 
+} 
 
 ReactDOM.render(
   <HelloWorld/>,
