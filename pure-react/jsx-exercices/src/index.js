0bd5672d185f68e44baa465407bdc6675c93a568
Yoric Mangeart <yoricm@free.fr>
2020-04-05 23:41:09 +0200

    Component with book tag

--- pure-react/jsx-exercices/src/index.js
+++ pure-react/jsx-exercices/src/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function MyThing () {
-
+  return (
+    <div className='book'>
+      <div className='title'>
+        The Title
+      </div>
+      <div className='author'>
+        The Author
+      </div>
+      <ul className='stats'>
+        <li className='rating'>
+          5 stars
+        </li>
+        <li className='isbn'>
+          12-345678-910
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 ReactDOM.render(
   <MyThing/>,
-  document.querySelector('root')
+  document.querySelector('#root')
 );
