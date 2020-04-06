cc647410b9950e649d89ac2ead4d3150b1dfe046
Yoric Mangeart <yoricm@free.fr>
2020-04-06 12:30:57 +0200

    Second version with createElement

--- pure-react/jsx-exercices/src/index.js
+++ pure-react/jsx-exercices/src/index.js
@@ -22,7 +22,40 @@ function MyThing () {
   );
 }
 
+function MyThing2() {
+  return (
+    React.createElement(
+      'div',
+      { className: 'book' },
+      React.createElement(
+        'div',
+        { className: 'title' },
+          'The Title 2'
+      ),
+      React.createElement(
+        'div',
+        { className: 'author' },
+        'The Author'
+      ),
+      React.createElement(
+        'ul',
+        { className: 'stats' },
+        React.createElement(
+          'li',
+          { className: 'rating' },
+          '5 stars'
+        ),
+        React.createElement(
+          'li',
+          { className: 'isbn' },
+          '12-345678-910'
+        )
+      )
+    )
+  );
+}
+
 ReactDOM.render(
-  <MyThing/>,
+  <MyThing2/>,
   document.querySelector('#root')
 );
